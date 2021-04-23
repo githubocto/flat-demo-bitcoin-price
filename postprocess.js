63df7b82d7de71b787fcc2768cdf001678067dcb
Irene Alvarado <irealva@users.noreply.github.com>
2021-04-23 15:12:44 -0700

    Update postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -2,7 +2,7 @@
 
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, json, excel, zip, and image files
-import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.8/mod.ts' 
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.9/mod.ts' 
 
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
