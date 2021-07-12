de500f0834aa1577ae9b8b858a64e82eb1d5c492
Irene Alvarado <irealva@users.noreply.github.com>
2021-07-12 10:32:21 -0400

    update Flat library version
    Fixing same issue that was presented here [https://github.com/githubocto/flat/issues/45](https://github.com/githubocto/flat/issues/45)

--- postprocess.js
+++ postprocess.js
@@ -2,7 +2,7 @@
 
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
-import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
+import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.11/mod.ts' 
 
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
